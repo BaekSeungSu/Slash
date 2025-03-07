724b8a4b5b49238acdd2ce58d4fb2bd33faf7003
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-03-07 14:49:14 +0900

    Bird 클래스 추가

---
+++ Source/Slash/Private/Pawns/Bird.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawns/Bird.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+ABird::ABird()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
+	Capsule->SetCapsuleHalfHeight(20.f);
+	Capsule->SetCapsuleRadius(15.f);
+	RootComponent = Capsule;
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	BirdMesh->SetupAttachment(Capsule);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ABird::MoveForward);
+}
+
+void ABird::MoveForward(float Value)
+{
+	if(Controller &&(Value != 0.f))
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, Value);
+	}
+}
